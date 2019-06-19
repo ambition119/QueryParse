528fa146f0345a13b86c5df39c52e32f6277bbfb
wpl <1269223860@qq.com>
2019-06-19 10:10:37 +0800

    calcite module

---
+++ calcite/src/test/java/com/ambition/sql/parser/ddl/SqlCreateTableTest.java
@@ -0,0 +1,47 @@
+package com.ambition.sql.parser.ddl;
+
+
+import static org.apache.calcite.sql.parser.SqlParser.DEFAULT_IDENTIFIER_MAX_LENGTH;
+
+import com.ambition.sql.parser.impl.AmSqlParserImpl;
+import java.io.StringReader;
+import org.apache.calcite.config.Lex;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlNodeList;
+import org.junit.Test;
+
+public class SqlCreateTableTest {
+  @Test
+  public void testCreateTable() throws Exception {
+    String ddl = "CREATE TABLE tbl1 (\n" +
+        "  a bigint,\n" +
+        "  h varchar, \n" +
+        "  g as 2 * (a + 1), \n" +
+        "  ts as timestamp, \n" +
+        "  b varchar,\n" +
+        "  proc as PROCTIME(), \n" +
+        "  PRIMARY KEY (a, b)\n" +
+        ")\n" +
+        "PARTITIONED BY (a, h)\n" +
+        "WITH (\n" +
+        "    connector = 'kafka', \n" +
+        "    kafka.topic = 'log.test'\n" +
+        ")\n";
+    StringReader in = new StringReader(ddl);
+    AmSqlParserImpl impl = new AmSqlParserImpl(in);
+    impl.switchTo("BTID");
+    impl.setTabSize(1);
+    impl.setQuotedCasing(Lex.JAVA.quotedCasing);
+    impl.setUnquotedCasing(Lex.JAVA.unquotedCasing);
+    impl.setIdentifierMaxLength(DEFAULT_IDENTIFIER_MAX_LENGTH);
+
+    SqlNode node = impl.parseSqlStmtEof();
+    if (node instanceof SqlCreateTable) {
+      SqlCreateTable createTable = (SqlCreateTable) node;
+      SqlNodeList columnList = createTable.getColumnList();
+      SqlNodeList propertyList = createTable.getPropertyList();
+    }
+
+  }
+
+}
